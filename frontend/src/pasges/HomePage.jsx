578a48b6e260d2a99d2caf6f8028ac1bbf38fbf1
kamal singh <kamalsingh902752@gmail.com>
2025-05-09 00:39:19 +0530

    Project Completed

--- frontend/src/pasges/HomePage.jsx
+++ frontend/src/pasges/HomePage.jsx
@@ -1,7 +1,7 @@
 import {useChatStore} from "../store/useChatStore.js";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
-import ChatContainer from "../components/chatContainer";
+import ChatContainer from "../components/ChatContainer";
 
 
 const HomePage = () => {
